fe0d312fe0a8bcfaa36252e2fe3f1da7c3329537
Candlemancer <Candlemancer@gmail.com>
2014-09-27 19:11:31 -0600

    Finished Hello World MIPS

--- CS 3810 - Computer Architecture/Hello MIPS/hello.asm
+++ CS 3810 - Computer Architecture/Hello MIPS/hello.asm
@@ -4,8 +4,16 @@
 
 .data
 
-	.asciiz "Hello World! My name is Jonathan Petersen and my major is Computer Science!"
+string:	.asciiz	"Hello World! My name is Jonathan Petersen and my major is Computer Science!"
 	
 .text
 
-	
+#syscall executes the command specified by $v0 and passes in the argument in $a0
+li	$v0	4
+la	$a0	string
+syscall
+
+#syscall 10 exits the program.
+li	$v0	10
+syscall
+
