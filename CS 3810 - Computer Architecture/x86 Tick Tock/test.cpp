971757650479c3442a5e9da094998a45b894e101
Candlemancer <Candlemancer@gmail.com>
2014-10-07 23:22:46 -0600

    Finished MIPS Guess a Number

---
+++ CS 3810 - Computer Architecture/x86 Tick Tock/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	string s = "Hello!";
+
+	if (s == "World!") {
+		cout << "What?" << endl;
+	}
+
+	cout << "Done!" << endl;
+	return 0;
+
+}
