284b8cf450ff0d934e2906ba141eeb54a5ac0b27
Candlemancer <Candlemancer@gmail.com>
2015-01-28 22:17:29 -0700

    Finished Fermat's Primality Testing

--- Python and Perl - CS 3430/Fermat's Primality Theorem/Fermat Primality.py
+++ Python and Perl - CS 3430/Fermat's Primality Theorem/Fermat Primality.py
@@ -5,6 +5,8 @@
 import random;
 random.seed();
 
+# Test whether or not a number is time. Note that this test is necessary but not sufficient to prove
+# primality, since there are numbers that will always fool this test.
 def isFermatPrime(number, repetitions, test = 2) :
 
 	if (number == 1 or number == 2) :
@@ -19,6 +21,20 @@ def isFermatPrime(number, repetitions, test = 2) :
 
 	return False;
 
-# def findFermatPrimes(numbers, repetitions, test = 2) :
+print isFermatPrime(9277, 20);
 
-print isFermatPrime(9275, 20);
+# Find primes in a list of numbers using the Fermat primality test.
+def findFermatPrimes(numbers, repetitions, test = 2) :
+
+	if (len(numbers) == 0) :
+		return [];
+
+	currentNumber = numbers.pop();
+
+	if (isFermatPrime(currentNumber, repetitions, test)) :
+		return ( findFermatPrimes(numbers, repetitions, test) + [currentNumber] );
+
+	return findFermatPrimes(numbers, repetitions, test);
+
+print findFermatPrimes([1, 2, 3, 4, 5, 6, 7, 8, 9], 20);
+
